1785988d16862890dfb3e4f222edc8103d02019e
anshu agrawal <anshu099.agrawal@gmail.com>
2017-09-15 21:18:58 +0530

    Shopping Page

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
 
-import { Item } from '../../models/item';
-import { Items } from '../../providers/providers';
+import { TranslateService } from '@ngx-translate/core';
+
+export interface Slide {
+  title: string;
+  description: string;
+  image: string;
+}
 
 @IonicPage()
 @Component({
@@ -10,32 +15,112 @@ import { Items } from '../../providers/providers';
   templateUrl: 'search.html'
 })
 export class SearchPage {
+  slides: Slide[];
+  items;
+  lists;
+  showSkip = true;
+  dir: string = 'ltr';
 
-  currentItems: any = [];
+  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public platform: Platform) {
+    this.dir = platform.dir();
+    translate.get(["TUTORIAL_SLIDE1_TITLE",
+      "TUTORIAL_SLIDE1_DESCRIPTION",
+      "TUTORIAL_SLIDE2_TITLE",
+      "TUTORIAL_SLIDE2_DESCRIPTION",
+      "TUTORIAL_SLIDE3_TITLE",
+      "TUTORIAL_SLIDE3_DESCRIPTION",
+    ]).subscribe(
+      (values) => {
+        console.log('Loaded values', values);
+        this.slides = [
+          {
+            title: values.TUTORIAL_SLIDE1_TITLE,
+            description: values.TUTORIAL_SLIDE1_DESCRIPTION,
+            image: 'assets/img/ica-slidebox-img-4.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE2_TITLE,
+            description: values.TUTORIAL_SLIDE2_DESCRIPTION,
+            image: 'assets/img/ica-slidebox-img-4.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE3_TITLE,
+            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            image: 'assets/img/ica-slidebox-img-4.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE3_TITLE,
+            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            image: 'assets/img/ica-slidebox-img-4.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE3_TITLE,
+            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            image: 'assets/img/ica-slidebox-img-4.png',
+          }
+        ];
+      });
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
+      this.items = [
+        "Lorem Ipsum 1",
+        "Lorem Ipsum 2",
+        "Lorem Ipsum 3"
+      ]
 
-  /**
-   * Perform a service for the proper items.
-   */
-  getItems(ev) {
-    let val = ev.target.value;
-    if (!val || !val.trim()) {
-      this.currentItems = [];
-      return;
-    }
-    this.currentItems = this.items.query({
-      name: val
+      this.lists = [{ 
+          "imageSrc": "assets/img/marty-avatar.png",
+          "content1": "Discount",
+          "date": "2017-08-19 14:07:12",
+          "dis": "20%",
+          "off": "off",
+          "ip": "Your IP: 100.82.199.127"
+        },
+        { 
+          "imageSrc": "assets/img/marty-avatar.png",
+          "content1": "Discount",
+          "date": "2017-08-19 14:07:12",
+          "dis": "20%",
+          "off": "off",
+          "ip": "Your IP: 100.82.199.127"
+        },
+        { 
+          "imageSrc": "assets/img/marty-avatar.png",
+          "content1": "Discount",
+          "date": "2017-08-19 14:07:12",
+          "dis": "20%",
+          "off": "off",
+          "ip": "Your IP: 100.82.199.127"
+        },
+        { 
+          "imageSrc": "assets/img/marty-avatar.png",
+          "content1": "Discount",
+          "date": "2017-08-19 14:07:12",
+          "dis": "20%",
+          "off": "off",
+          "ip": "Your IP: 100.82.199.127"
+        }
+      ]
+  }
+
+  startApp() {
+    this.navCtrl.setRoot('WelcomePage', {}, {
+      animate: true,
+      direction: 'forward'
     });
   }
 
-  /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
-    });
+  onSlideChangeStart(slider) {
+    this.showSkip = !slider.isEnd();
+  }
+
+  ionViewDidEnter() {
+    // the root left menu should be disabled on the tutorial page
+    this.menu.enable(false);
+  }
+
+  ionViewWillLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menu.enable(true);
   }
 
 }
