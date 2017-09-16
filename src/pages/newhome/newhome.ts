17fcc4eb7618b6b3c85f96becceecfa09a88a544
sauravhny <sauravs012@gmail.com>
2017-09-16 10:56:32 +0530

    first commit with 5 new pages

---
+++ src/pages/newhome/newhome.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-newhome',
+  templateUrl: 'newhome.html'
+})
+export class NewhomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/setting/setting.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PopoverController } from 'ionic-angular';
+import { PopoverPage } from '../popover/popover';
+/**
+ * Generated class for the SettingPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-setting',
+  templateUrl: 'setting.html',
+})
+export class SettingPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SettingPage');
+  }
+  
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
+  }
+
+}
