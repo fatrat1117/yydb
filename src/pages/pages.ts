48d9dd143eab5f5045967b140c25c12dc4e41cd4
anshu <anshu099.agrawal@gmail.com>
2017-09-19 19:22:07 +0530

    changes in search,tabs and table page

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -11,3 +11,4 @@ export const Tab2Root = 'SearchPage';
 export const Tab3Root = 'SettingsPage';
 export const Tab4Root = 'MinePage';
 export const Tab5Root = 'ApiTestsPage';
+export const Tab6Root = 'SearchPage';
