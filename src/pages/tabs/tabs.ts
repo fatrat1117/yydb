6b1fdab91f7f5ec6756664e771bf11385d091eb6
fatrat1117 <hao.li@razerzone.com>
2017-09-20 11:19:15 +0800

    remove duplicated tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -20,7 +20,7 @@ export class TabsPage {
   tab3Root: any = Tab3Root;
   tab4Root: any = Tab4Root;
   tab5Root: any = Tab5Root;
-  tab6Root: any = Tab6Root;
+  //tab6Root: any = Tab6Root;
   constructor(public navCtrl: NavController, public translateService: TranslateService) {
     // translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
 
